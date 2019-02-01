6a2ec4d09bf5256e6e200a8e62a8229d18f578f9
Yasuhiro Nitta <lp6m2014@gmail.com>
2019-02-01 11:46:41 +0900

    FPT 2018 contest version

--- sw/src/realtime3.cpp
+++ sw/src/realtime3.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
-#include <chrono>
 #include <map>
 #include <vector>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <chrono>
+#include <linux/videodev2.h>
 
 #include <opencv2/opencv.hpp>
 #include <opencv2/core.hpp>
@@ -12,12 +20,31 @@
 #include "forest.h"
 #include "window_candidate.h"
 #include "hw.h"
+
+
+#define REQUEST_BUFFER_NUM 10
+
+static int xioctl(int fd, int request, void *arg)
+{
+    int r;
+    do {
+        r = ioctl (fd, request, arg);
+        if (request == VIDIOC_DQBUF) {
+            std::cout << "r : " << r << std::endl;
+        }
+    } while (-1 == r && EINTR == errno);
+    return r;
+}
+unsigned char *buffers[4];
+
 using namespace std;
 using namespace cv;
 
 map<int, vector<pair<int, int> > > mp;
 map<int, vector<pair<int, int> > > original_mp;
 set<int> widthkind;
+
+int got_buffer_num;
 
 int sx_min = 999;
 int sy_min = 999;
@@ -257,6 +284,113 @@ vector<pair<vector<int>, float>> test_one_frame(Mat frame){
     return rst;
 }
 
+void camera_setup(int fd){
+    // 2. Querying video capabilities.
+    struct v4l2_capability caps;
+    memset(&caps, 0, sizeof(caps));
+    if (-1 == xioctl(fd, VIDIOC_QUERYCAP, &caps))
+    {
+        std::cout << "Failed to query capabilities." << std::endl;
+        return 1;
+    }
+    std::cout << "bus_info  : " << caps.bus_info << std::endl;
+    std::cout << "card      : " << caps.card << std::endl;
+    std::cout << "driver    : " << caps.driver << std::endl;
+    std::cout << "version   : " << caps.version << std::endl;
+
+    // 3. Format Specification.
+    {
+        struct v4l2_format fmt;
+        memset(&(fmt), 0, sizeof(fmt));
+
+        fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        fmt.fmt.pix.width = 640;
+        fmt.fmt.pix.height = 480;
+        fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_YUYV;
+        fmt.fmt.pix.field = V4L2_FIELD_NONE;
+
+        if (-1 == xioctl(fd, VIDIOC_S_FMT, &fmt))
+        {
+            std::cout << "Failed to set pixel format." << std::endl;
+            return 1;
+        }
+    }
+
+    // 4. Request Buffer
+    {
+        struct v4l2_requestbuffers req;
+        memset(&(req), 0, sizeof(req));
+        req.count = REQUEST_BUFFER_NUM;
+        req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        req.memory = V4L2_MEMORY_MMAP;
+
+        if (-1 == xioctl(fd, VIDIOC_REQBUFS, &req))
+        {
+            std::cout << "Failed to request buffer." << std::endl;
+            return 1;
+        }
+        cout << "we could get " << req.count  << " buffers. " << endl;
+        got_buffer_num = req.count;
+    }
+
+    for(auto itr = widthkind.begin(); itr != widthkind.end(); ++itr) {
+        int width = *itr;
+        cout << width << ":" << mp[width].size() << endl;
+    }
+
+    // 5. Query Buffer
+    {
+        for(int bufferindex = 0; bufferindex < got_buffer_num; bufferindex++){
+            struct  v4l2_buffer buf;
+            memset(&(buf), 0, sizeof(buf));
+            buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+            buf.memory = V4L2_MEMORY_MMAP;
+            buf.index = bufferindex;
+            if(-1 == xioctl(fd, VIDIOC_QUERYBUF, &buf))
+            {
+                std::cout << "Failed to query buffer." << std::endl;
+                return 1;
+            }
+
+            std::cout << "buf.length : " << buf.length << std::endl;
+            std::cout << "buf.m.offset : " << buf.m.offset << std::endl;
+
+            buffers[bufferindex] = (unsigned char*)mmap (NULL, buf.length, PROT_READ | PROT_WRITE, MAP_SHARED, fd, buf.m.offset);
+            if (MAP_FAILED == buffers[bufferindex])
+                cerr << "mmap" << endl;
+        }
+    }
+
+    for(auto itr = widthkind.begin(); itr != widthkind.end(); ++itr) {
+        int width = *itr;
+        cout << width << ":" << mp[width].size() << endl;
+    }
+
+    
+    // 5.5 QBUF Request
+    {
+        for (int i = 0; i < got_buffer_num; ++i) {
+            struct v4l2_buffer buf;
+            buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+            buf.memory = V4L2_MEMORY_MMAP;
+            buf.index = i;
+            if (-1 == xioctl(fd, VIDIOC_QBUF, &buf))
+                cerr << "VIDIOC_QBUF" << endl;
+        }
+    }
+
+    // 6. Start Streaming
+    {
+        struct  v4l2_buffer buf;
+        memset(&(buf), 0, sizeof(buf));
+        buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        if(-1 == xioctl(fd, VIDIOC_STREAMON, &buf.type))
+        {
+            std::cout << "Start Capture" << std::endl;
+            return 1;
+        }
+    }
+}
 int main(int argc, const char* argv[]){
 
     check_window();
@@ -264,16 +398,72 @@ int main(int argc, const char* argv[]){
     cout << "hw_setup" << endl;
     if(hwmode) hw_setup();
 
+    int fd;
+    fd = open("/dev/video1", O_RDWR, 0);
+    if (fd == -1){
+        std::cout << "Failed to open video device." << std::endl;
+        return 1;
+    }
     for(auto itr = widthkind.begin(); itr != widthkind.end(); ++itr) {
         int width = *itr;
         cout << width << ":" << mp[width].size() << endl;
     }
-	std::chrono::system_clock::time_point  t1, t2, t3, t4, t5, t6, t7;
-	cv::VideoCapture cap(1);
-	while(1){
+    camera_setup(fd);
+
+    for(auto itr = widthkind.begin(); itr != widthkind.end(); ++itr) {
+        int width = *itr;
+        cout << width << ":" << mp[width].size() << endl;
+    }
+    std::chrono::system_clock::time_point  t1, t2, t3, t4, t5, t6, t7;
+    // cv::VideoCapture cap(1);
+    // char camdata[640*480*2];
+    cv::Mat rawimg(480, 640, CV_8UC2);
+    // cv::Mat bgrimg(480, 640, CV_8UC2);
+
+    while(1){
         t1 = std::chrono::system_clock::now();
+        {
+            {
+                struct v4l2_buffer buf;
+                buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+                buf.memory = V4L2_MEMORY_MMAP;
+                fd_set fds;
+                FD_ZERO(&fds);
+                FD_SET(fd, &fds);
+                struct timeval tv = {0};
+                tv.tv_sec = 2;
+                int r = select(fd+1, &fds, NULL, NULL, &tv);
+
+                if(-1 == r){
+                    std::cout << "Waiting for Frame" << std::endl;
+                    return 1;
+                }
+        
+                memset(&(buf), 0, sizeof(buf));
+                buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+                buf.memory = V4L2_MEMORY_MMAP;
+
+                if(-1 == xioctl(fd, VIDIOC_DQBUF, &buf))
+                {
+                    std::cout << "Retrieving Frame" << std::endl;
+                    return 1;
+                }
+
+                cout << "buf.index : " << buf.index << endl;
+            
+                // Connect buffer to queue for next capture.
+                if (-1 == xioctl(fd, VIDIOC_QBUF, &buf)) {
+                    std::cout << "VIDIOC_QBUF" << std::endl;
+                }
+                memcpy(rawimg.data, buffers[buf.index], 640 * 480 * 2);
+            }
+
+        }
+
+        // memcpy(rawimg.data, camdata, 640 * 480 * 2);
         cv::Mat frame;
-        cap >> frame; // get a new frame from camera
+        cv::cvtColor(rawimg, frame, cv::COLOR_YUV2BGR_YUYV);
+        // cap >> frame; // get a new frame from camera
         cv::Mat frame_copy = frame.clone();
 
         //process the current frame
@@ -291,6 +481,10 @@ int main(int argc, const char* argv[]){
             rectangle(frame_copy, Point(sx, sy), Point(ex, ey), Scalar(0,0,200), 2); //x,y //Scaler = B,G,R
             cv::putText(frame_copy, to_string(proba), cv::Point(sx+5,sy+5), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255,0,0), 1, CV_AA);
         }
+        if(!hwmode){
+            cv::imshow("result", frame_copy);
+            cv::waitKey(1);
+        }
 		t2 = std::chrono::system_clock::now();
 		//show fps
         double elapsed = (double)std::chrono::duration_cast<std::chrono::milliseconds>(t2-t1).count();
@@ -299,5 +493,25 @@ int main(int argc, const char* argv[]){
 	}
 
     if(hwmode) hw_release();
+
+    // 10. Turn off streaming.
+    struct v4l2_buffer buf;
+    buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    buf.memory = V4L2_MEMORY_MMAP;
+    if (-1 == xioctl(fd, VIDIOC_STREAMOFF, &buf.type)) {
+        std::cout << "VIDIOC_STREAMOFF" << std::endl;
+    }
+    std::cout << "unko" << endl;
+    // 11. munmap
+    for(int i = 0; i < got_buffer_num; i++){
+        if(munmap(buffers[i], 480 * 640 * 2) != 0){
+            cerr << "munmap failed" << endl;
+        }else{
+            cout << "munmap success" << endl;
+        }
+    }
+    // 12.close device
+    close(fd);
+
     return 0;
 }
